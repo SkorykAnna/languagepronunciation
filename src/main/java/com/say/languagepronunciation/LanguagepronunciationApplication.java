9dffc2d19a434dfe808ad624d1076577b05c657b
Anna Skoryk <skoryk.a.y@gmail.com>
2023-09-12 14:39:16 +0200

    Initial commit

---
+++ src/main/java/com/say/languagepronunciation/LanguagepronunciationApplication.java
@@ -0,0 +1,13 @@
+package com.say.languagepronunciation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagepronunciationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagepronunciationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/say/languagepronunciation/LanguagepronunciationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.say.languagepronunciation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagepronunciationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
